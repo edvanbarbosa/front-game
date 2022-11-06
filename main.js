321122ced1d13db9e30ac46e3b69a36930a64bf0
Edvan Barbosa <114129267+edvanbarbosa@users.noreply.github.com>
2022-11-06 07:12:31 -0300

    ....

--- main.js
+++ main.js
@@ -5,17 +5,45 @@ var background_c = undefined
 console.log(background_c)
 
 select_p.forEach(e =>{
-    
     e.addEventListener('click',()=>{
         player_name.innerHTML = e.innerHTML
-    })
-})
-
-colors.forEach(color =>{
-    color.addEventListener('click',function passo(){
-        background_c = color.style.background
-        p_select.style.background = background_c
         
-
     })
 })
+
+let p1_front = document.getElementById('p1_front')
+let p2_front = document.getElementById('p2_front')
+
+p1_front.addEventListener('click',()=>{
+    colors.forEach(color =>{
+        color.addEventListener('click',function passo(){
+            p1_front.style.background = color.style.background
+            
+    
+        })
+    })
+    
+    p1_front.style.border = '4px solid white'
+    p2_front.style.border = 'none'
+    p2_front.blur()
+
+}
+)
+
+p2_front.addEventListener('click',()=>{
+    colors.forEach(color =>{
+        color.addEventListener('click',function passo(){
+            p2_front.style.background = color.style.background
+            
+    
+        })
+    })
+    
+    p2_front.style.border = '4px solid white'
+    p1_front.style.border = 'none'
+
+}
+)
+
+
+
